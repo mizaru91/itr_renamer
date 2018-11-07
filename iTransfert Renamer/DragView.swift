d1ae115575c958ba1d3dd7e78ac004ee6af5801b
mizaru91 <31534130+mizaru91@users.noreply.github.com>
2018-11-07 23:06:22 +0100

    Add files via upload

---
+++ iTransfert Renamer/DragView.swift
@@ -0,0 +1,74 @@
+//
+//  DragView.swift
+//  iTransfert Renamer
+//
+//  Created by Mizaru on 01/11/2018.
+//  Copyright © 2018 France Televisions. All rights reserved.
+//
+
+import Cocoa
+
+protocol DragViewDelegate {
+    func dragView(didDragFileWith URL: NSURL)
+}
+
+class DragView: NSView {
+    
+    var delegate: DragViewDelegate?
+    //1
+    private var fileTypeIsOk = false
+    private var acceptedFileExtensions = ["jpg","png","mov"]
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        register(forDraggedTypes: [NSFilenamesPboardType])
+    }
+    
+    //2/
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        fileTypeIsOk = checkExtension(drag: sender)
+        return []
+    }
+    
+    //3
+    override func draggingUpdated(_ sender: NSDraggingInfo) -> NSDragOperation {
+        return fileTypeIsOk ? .copy : []
+    }
+    
+    //4
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        guard let draggedFileURL = sender.draggedFileURL else {
+            return false
+        }
+        
+        //call the delegate
+        if fileTypeIsOk {
+            delegate?.dragView(didDragFileWith: draggedFileURL)
+        }
+        
+        return true
+    }
+    
+    //5
+    fileprivate func checkExtension(drag: NSDraggingInfo) -> Bool {
+        guard let fileExtension = drag.draggedFileURL?.pathExtension?.lowercased() else {
+            return false
+        }
+        
+        return acceptedFileExtensions.contains(fileExtension)
+    }
+    override func hitTest(_ aPoint: NSPoint) -> NSView? {
+        return nil
+    }
+    
+}
+
+//6
+extension NSDraggingInfo {
+    var draggedFileURL: NSURL? {
+        let filenames = draggingPasteboard().propertyList(forType: NSFilenamesPboardType) as? [String]
+        let path = filenames?.first
+        return path.map(NSURL.init)
+    }
+}
+
---
+++ iTransfert Renamer/HomeViewController.swift
@@ -0,0 +1,222 @@
+//
+//  ViewController.swift
+//  iTransfert Renamer
+//
+//  Created by Mizaru on 01/11/2018.
+//  Copyright © 2018 France Televisions. All rights reserved.
+//
+
+import Cocoa
+//import Foundation
+
+class HomeViewController: NSViewController, NSComboBoxDelegate {
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var staticLabel: NSTextField!
+    @IBOutlet weak var dragView: DragView!
+    @IBOutlet weak var titleTxt: NSTextField!
+    @IBOutlet weak var versionMenu: NSPopUpButton!
+    @IBOutlet weak var redaMenu: NSComboBox!
+    @IBOutlet weak var jriTxt: NSTextField!
+    @IBOutlet weak var editionMenu: NSComboBox!
+    @IBOutlet weak var lieuMenu: NSTextField!
+    @IBOutlet weak var showFilepath: NSTextField!
+    var filepath: NSURL?
+    var titleSelected: String?
+    var redaSelected: String?
+    var jriSelected: String?
+    var lieuSelected: String?
+    
+    let toUppercaseASCIINoSpaces = StringTransform(rawValue: "Latin-ASCII; Upper; [:Separator:] Remove; [:Punctuation:] Remove; [:Mark:] Remove;[:Symbol:] Remove;")
+   
+    @IBAction func renameFile(_ sender: NSButton) {
+        
+        // On récupère la valeur du champs TITRE
+        var title: String? = titleTxt.stringValue
+        if titleTxt.stringValue.isEmpty {
+            title = "NULL"
+        }
+        titleSelected = sanitise(texte: title)
+        
+        // On récupère la valeur du champs JRI
+        var jri: String? = jriTxt.stringValue
+        if jriTxt.stringValue.isEmpty {
+            jri = "NULL"
+        }
+        jriSelected = sanitise(texte: jri)
+        
+        // On récupère la valeur du champs JRI
+        var lieu: String? = lieuMenu.stringValue
+        if lieuMenu.stringValue.isEmpty {
+            lieu = "NULL"
+        }
+        lieuSelected = sanitise(texte: lieu)
+        
+        // On récupère la valeur du menu VERSION
+        var vers: String? = versionMenu.titleOfSelectedItem
+        if versionMenu.titleOfSelectedItem!.isEmpty {
+            vers = "NULL"
+        }
+        let versionSelected: String? = sanitise(texte: vers)
+        
+        // On récupère la valeur du menu REDACTION
+        var redac: String? = redaMenu.stringValue
+        if redaMenu.stringValue.isEmpty {
+                redac = "NULL"
+        }
+        redaSelected = sanitise(texte: redac)
+        
+        // On récupère la valeur du menu EDITION
+        var edit: String? = editionMenu.stringValue
+        if editionMenu.stringValue.isEmpty {
+            edit = "NULL"
+        }
+        let editSelected: String? = sanitise(texte: edit)
+        
+        // On teste la présence d'un fichier dropé avant de travailler dessus.
+        if filepath != nil {
+            /*:     Phase de renommage du fichier, si le chemin du fichier est connu
+             Create a FileManager instance                                          */
+            let fileManager = FileManager.default
+            if fileManager.fileExists(atPath: filepath!.path!){
+                do {
+                    // Nomenclature = F3_12-13_V1_MON_TITRE_CHRISTOPH_KAR_COMBS_LA_VILLE___135K
+                    // Extraction du chemin
+                    let path: String = filepath!.deletingLastPathComponent!.absoluteString
+                    // Extrraction de l'extension
+                    let ext: String = filepath!.pathExtension!
+                    let renamedpath: String = ("\(path)\(redaSelected!)_\(titleSelected!)_\(jriSelected!)_\(lieuSelected!)__\(infoAbout(url: filepath as! URL)).\(ext)")
+                    try fileManager.moveItem(at: filepath as! URL, to: URL(string : renamedpath)!)
+                } catch let error as NSError {
+                    showModal(alerte: "\(error)")
+                }
+        } else {
+                showModal(alerte: "Fichier introuvable, merci de vérifier le chemin: \(filepath!.path!)")
+            //NSLog(self.filepath!.absoluteString!)
+            }
+        } else {
+            showModal(alerte: "Chemin inconnu...")
+        }
+    }
+    
+    
+    // Mes fonctions
+    func sanitise(texte: String?) -> String {
+        let sanitised: String? = texte!.applyingTransform(toUppercaseASCIINoSpaces, reverse: false)!
+            .applyingTransform(.stripCombiningMarks, reverse: false)!
+            .applyingTransform(.stripDiacritics, reverse: false)!
+        NSLog(sanitised!)
+        return sanitised!
+    }
+    func showModal(alerte: String?){
+        let alert = NSAlert()
+        alert.alertStyle = .critical
+        alert.messageText = "Erreur"
+        alert.informativeText = alerte!
+        alert.runModal()
+    }
+    // Fonction de récupération des informations techniques du fichier
+    func infoAbout(url: URL) -> String {
+        // 1
+        let fileManager = FileManager.default
+        var imgSizeInMB: String?
+        // 2
+        do {
+            // 3
+            let attributes = try fileManager.attributesOfItem(atPath: url.path)
+            //var report: [String] = ["\(url.path)", ""]
+            var report: String = url.path
+            // 4
+            for (key, value) in attributes {
+                // ignore NSFileExtendedAttributes as it is a messy dictionary
+                if key.rawValue == "NSFileExtendedAttributes" { continue }
+                // Filtre juste sur la taille de fichier, les autres attributs ne sont pas util
+                /*: NSFileOwnerAccountName:     mizaru
+                NSFilePosixPermissions:     420
+                NSFileSystemNumber:     16777223
+                NSFileReferenceCount:     1
+                NSFileSystemFileNumber:     1909228
+                NSFileCreationDate:     2018-10-05 19:05:15 +0000
+                NSFileHFSTypeCode:     0
+                NSFileType:     NSFileTypeRegular
+                NSFileGroupOwnerAccountName:     staff
+                NSFileGroupOwnerAccountID:     20
+                NSFileHFSCreatorCode:     0
+                NSFileModificationDate:     2018-10-05 19:05:15 +0000
+                NSFileSize:     137895
+                NSFileExtensionHidden:     1
+                NSFileOwnerAccountID:     503 */
+                if key.rawValue == "NSFileSize" {
+                    imgSizeInMB = format(bytes: Double(value as! Int))
+                    //print("Taille du fichier:\(imgSizeInMB)")
+                    //report.append("\(key.rawValue):\t \(value)")
+                }
+            }
+            // 5
+            //return report.joined(separator: "\n")
+            return imgSizeInMB!
+        } catch {
+            // 6
+            return "No information available for \(url.path)"
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        dragView.delegate = self
+        // Combobox Rédaction
+        versionMenu.removeAllItems()
+        versionMenu.addItems(withTitles: ["V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10"])
+        // Combobox Rédaction
+        redaMenu.removeAllItems()
+        redaMenu.addItems(withObjectValues: ["France2", "France3", "FranceInfo"])
+        // Combobox Edition
+        editionMenu.removeAllItems()
+        editionMenu.addItems(withObjectValues: ["12-13", "13h", "19-20", "20h", "Soir3", "Rushes", "Elements", "Internet", "Telematin", "Week-end", "Autre"])
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    func format(bytes: Double) -> String {
+        guard bytes > 0 else {
+            return "0 bytes"
+        }
+        
+        // Adapted from http://stackoverflow.com/a/18650828
+        let suffixes = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+        let k: Double = 1000
+        let i = floor(log(bytes) / log(k))
+        
+        // Format number with thousands separator and everything below 1 GB with no decimal places.
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = i < 3 ? 0 : 1
+        numberFormatter.numberStyle = .decimal
+        
+        let numberString = numberFormatter.string(from: NSNumber(value: bytes / pow(k, i))) ?? "Unknown"
+        let suffix = suffixes[Int(i)]
+        return "\(numberString)\(suffix)"
+    }
+
+}
+
+extension Optional where Wrapped == String {
+    var nilIfEmpty: String? {
+        guard let strongSelf = self else {
+            return nil
+        }
+        return strongSelf.isEmpty ? nil : strongSelf
+    }
+}
+extension HomeViewController: DragViewDelegate {
+    func dragView(didDragFileWith URL: NSURL) {
+        self.filepath = URL
+        self.showFilepath.stringValue = URL.path ?? "Désolé, information de chemin, non disponible"
+        //print(self.filepath ?? "Désolé, information de chemin, non disponible")
+    }
+}
+//extension StringTransform {
+//    static let toUppercaseASCIINoSpaces = StringTransform(rawValue: "Latin-ASCII; Lower; [:Separator:] Remove;")
+//}
