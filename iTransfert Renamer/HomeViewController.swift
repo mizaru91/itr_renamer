f8b3a9cfa6dddfa408231c29ba4134c7149cde49
Mizaru <mizaru@imac-mizaru.local>
2018-11-10 02:51:13 +0100

    added textfield delegate actions

--- iTransfert Renamer/HomeViewController.swift
+++ iTransfert Renamer/HomeViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 //import Foundation
 
-class HomeViewController: NSViewController, NSComboBoxDelegate {
+class HomeViewController: NSViewController, NSComboBoxDelegate, NSTextFieldDelegate {
     @IBOutlet weak var imageView: NSImageView!
     @IBOutlet weak var staticLabel: NSTextField!
     @IBOutlet weak var dragView: DragView!
@@ -20,11 +20,13 @@ class HomeViewController: NSViewController, NSComboBoxDelegate {
     @IBOutlet weak var editionMenu: NSComboBox!
     @IBOutlet weak var lieuMenu: NSTextField!
     @IBOutlet weak var showFilepath: NSTextField!
+    @IBOutlet weak var showSanitisedName: NSTextField!
     var filepath: NSURL?
     var titleSelected: String?
     var redaSelected: String?
     var jriSelected: String?
     var lieuSelected: String?
+    
     
     let toUppercaseASCIINoSpaces = StringTransform(rawValue: "Latin-ASCII; Upper; [:Separator:] Remove; [:Punctuation:] Remove; [:Mark:] Remove;[:Symbol:] Remove;")
    
@@ -159,7 +161,79 @@ class HomeViewController: NSViewController, NSComboBoxDelegate {
             return "No information available for \(url.path)"
         }
     }
-
+    func getUserInputs() {
+        // On récupère la valeur du champs TITRE
+        var title: String? = titleTxt.stringValue
+        if titleTxt.stringValue.isEmpty {
+            title = "NULL"
+        }
+        titleSelected = sanitise(texte: title)
+        
+        // On récupère la valeur du champs JRI
+        var jri: String? = jriTxt.stringValue
+        if jriTxt.stringValue.isEmpty {
+            jri = "NULL"
+        }
+        jriSelected = sanitise(texte: jri)
+        
+        // On récupère la valeur du champs JRI
+        var lieu: String? = lieuMenu.stringValue
+        if lieuMenu.stringValue.isEmpty {
+            lieu = "NULL"
+        }
+        lieuSelected = sanitise(texte: lieu)
+        
+        // On récupère la valeur du menu VERSION
+        var vers: String? = versionMenu.titleOfSelectedItem
+        if versionMenu.titleOfSelectedItem!.isEmpty {
+            vers = "NULL"
+        }
+        let versionSelected: String? = sanitise(texte: vers)
+        
+        // On récupère la valeur du menu REDACTION
+        var redac: String? = redaMenu.stringValue
+        if redaMenu.stringValue.isEmpty {
+            redac = "NULL"
+        }
+        redaSelected = sanitise(texte: redac)
+        
+        // On récupère la valeur du menu EDITION
+        var edit: String? = editionMenu.stringValue
+        if editionMenu.stringValue.isEmpty {
+            edit = "NULL"
+        }
+        let editSelected: String? = sanitise(texte: edit)
+        
+        // On teste la présence d'un fichier dropé avant de travailler dessus.
+        if filepath != nil {
+            /*:     Phase de renommage du fichier, si le chemin du fichier est connu
+             Create a FileManager instance                                          */
+            let fileManager = FileManager.default
+            if fileManager.fileExists(atPath: filepath!.path!){
+                do {
+                    // Nomenclature = F3_12-13_V1_MON_TITRE_CHRISTOPH_KAR_COMBS_LA_VILLE___135K
+                    // Extraction du chemin
+                    let path: String = filepath!.deletingLastPathComponent!.absoluteString
+                    // Extrraction de l'extension
+                    let ext: String = filepath!.pathExtension!
+                    let renamedpath: String = ("\(redaSelected!)_\(titleSelected!)_\(jriSelected!)_\(lieuSelected!)__\(infoAbout(url: filepath as! URL)).\(ext)")
+                    //try fileManager.moveItem(at: filepath as! URL, to: URL(string : renamedpath)!)
+                    // On modifie la valeur du label pour afficher le nommage
+                    showSanitisedName.stringValue = renamedpath
+                } catch let error as NSError {
+                    NSLog("\(error)")
+                }
+            } else {
+                NSLog("Fichier introuvable, merci de vérifier le chemin: \(filepath!.path!)")
+            }
+        } else {
+            NSLog("Chemin inconnu...")
+        }
+    }
+    override func controlTextDidChange(_ obj: Notification) {
+        getUserInputs()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
